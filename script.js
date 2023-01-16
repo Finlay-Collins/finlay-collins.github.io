0d8891ccbd0b1596dcd72008b09b17d6022cbba1
Finlay Collins <58078471+Finlay-Collins@users.noreply.github.com>
2023-01-16 17:23:59 +0000

    finished

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ var quoteTable = [
     "Even monkeys can fall from trees", "Anonymous (Japanese Proverb)",
     "Appear weak when you are strong, and strong when you are weak", "Sun Tzu",
     "Fall seven times, stand up eight", "Anonymous (Japanese Proverb)",
-    "The best time to plant a tree was a hundred years ago. The second best time is now.", "Anonymous (Japanese Proverb)",
+    "The best time to plant a tree was a hundred years ago. The second best time is now.", "Anonymous (Chinese Proverb)",
     "Consideration for others is the basic of a good life, a good society.", "Kong Fuzi (Confucious)",
     "Everything has beauty, but not everyone sees it.", "Kong Fuzi (Confucious)",
     "Don't become a Buddhist: the world doesn't need more Buddhists. Do practice compassion: the world needs more compassion.", "Tenzin Gyatso (14th Dalai Lama)",
     "It is precisely when we have suffered defeat that we can determine to win and open the path to future victory.", "Daisaku Ikeda",
-    "Emptiness, which is conceptually liable to be mistaken for sheer nothingness, is in fact the reservoir of infinite possibilities.", "Daisetsu Teitaro Suzuki"
+    "Emptiness, which is conceptually liable to be mistaken for sheer nothingness, is in fact the reservoir of infinite possibilities.", "Daisetsu Teitaro Suzuki",
+    "The talented hawk hides his claws", "Anonymous (Japanese Proverb)",
+    "One who chases after two hares won't even catch one", "Anonymous (Japanese Proverb)",
+    "Happiness is not something ready-made. It comes from your own actions.", "Tenzin Gyatso (14th Dalai Lama)"
 ];
 
 const quote = document.querySelector("#quote");
@@ -20,7 +23,7 @@ function randint(max) {
     return Math.floor(Math.random() * max);
 }
 
-/* Prevent table from bugging out at boundaries by using *
+/* Prevent array from bugging out at boundaries by using *
  * different logic for odd and even */
 
 if (rand % 2 == 0) {
